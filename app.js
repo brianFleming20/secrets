269fa95afbd43248005240172b07b43a21b1691d
Brian Fleming <59600239+brianFleming-coder@users.noreply.github.com>
2020-12-18 17:28:05 +0000

    finished site

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-find-or-create');
+const findOrCreate = require('mongoose-findorcreate');
 
 
 
@@ -42,7 +42,8 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 
@@ -55,14 +56,24 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRETS,
-    callbackURL: "http://localhost:3000/auth/google/secrets-code",
-    userProfileURL: "https://www.googleapis.com/0auth2/v3/userinfo"
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/0auth20/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
 
@@ -75,14 +86,21 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+const personnelSite = "personnel site.";
+const commerse = "e-Commerse site.";
+const appSite = "application support site.";
+
 app.get("/", function(req, res) {
+  // personnal:personnelSite,
+  // commerse: commerse,
+  // application: appSite
   res.render("home");
 });
 
-app.get("/auth/google/",
-  passport.authenticate('google', {
-    scope: ["profile"]
-  }));
+app.get('/auth/google/',
+  passport.authenticate('google', { scope:
+      [  'https://www.googleapis.com/auth/userinfo.profile'] }
+));
 
 app.get("/auth/google/secrets-code",
   passport.authenticate("google", {
@@ -148,6 +166,53 @@ app.post("/login", function(req, res) {
 
 
 });
+
+
+
+app.get("/quotepersonnal", function(req, res){
+  res.render("quoteapp",{
+    content: personnelSite
+  });
+  
+  });
+  
+  app.get("/ecommerse", function(req, res){
+    res.render("quoteapp",{
+      content: commerse
+    });
+    
+    });
+  
+    app.get("/maintain", function(req, res){
+      res.render("maintain");
+    });
+  
+    app.get("/quoteapp", function(req, res){
+      res.render("quoteapp",{
+        content: appSite
+      });
+      
+      });
+  
+  app.get("/contact", function(req, res){
+    res.render("contact", function(err, signup){
+      if(!err){
+        res.render("success");
+      }
+    });
+  });
+  
+  app.get("/about", function(req, res){
+  res.render("about");
+  });
+
+  app.get("/gdpr", function(req, res){
+    res.render("dataProtection");
+  })
+  
+  app.use("/", (req, res, next) => {
+    res.render("404");
+  });
 
 
 app.listen(3000, function() {
